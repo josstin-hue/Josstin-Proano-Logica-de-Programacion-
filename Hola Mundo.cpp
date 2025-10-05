40498593e9c5aaeed1a6dcebf89592bd5b18971f
josstin-hue <josstinpes@gmail.com>
2025-10-05 15:42:18 -0500

    Delete Hola Mundo.cpp

--- Hola Mundo.cpp
+++
@@ -1,7 +0,0 @@
-
-print("Bienvenido al programa para concatenar nombre y apellido para formar el nombre completo");
-a=(input("Ingrese su nombre: "))
-b=(input("Ingrese su apellido: "))
-c=a+" "+b
-print("Su nombre completo es: ",c)
-
